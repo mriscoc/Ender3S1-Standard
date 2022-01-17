c30918a13641fdca407665af84d36c1a86f75585
Miguel Risco-Castillo <mriscoc@users.noreply.github.com>
2022-01-16 23:29:15 -0500

    Adjusted vertical position of button labels

--- Marlin/src/lcd/e3v2/enhanced/dwin.cpp
+++ Marlin/src/lcd/e3v2/enhanced/dwin.cpp
@@ -294,11 +294,11 @@ void ICON_Button(const bool selected, const int iconid, const frame_rect_t &ico,
   DWIN_ICON_Show(true, false, false, ICON, iconid + selected, ico.x, ico.y);
   if (selected) DWINUI::Draw_Box(0, HMI_data.Highlight_Color, ico);
   if (HMI_IsChinese()) {
-    DWIN_Frame_AreaCopy(1, txt.x, txt.y[selected], txt.x + txt.w - 1, txt.y[selected] + txt.h - 1, ico.x + (ico.w - txt.w) / 2, (ico.y + ico.h - 28) - txt.h/2);
+    DWIN_Frame_AreaCopy(1, txt.x, txt.y[selected], txt.x + txt.w - 1, txt.y[selected] + txt.h - 1, ico.x + (ico.w - txt.w) / 2, (ico.y + ico.h - 25) - txt.h/2);
   }
   else {
     const uint16_t x = ico.x + (ico.w - strlen_P(FTOP(caption)) * DWINUI::fontWidth()) / 2,
-                   y = (ico.y + ico.h - 28) - DWINUI::fontHeight() / 2;
+                   y = (ico.y + ico.h - 20) - DWINUI::fontHeight() / 2;
     DWINUI::Draw_String(x, y, caption);
   }
 }
