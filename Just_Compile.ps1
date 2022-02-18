a906e6d98b673993c33f2a96a1d54d4229086d1f
Miguel Risco-Castillo <mriscoc@users.noreply.github.com>
2022-02-18 00:56:17 -0500

    Update compile script

--- Just_Compile.ps1
+++ Just_Compile.ps1
@@ -8,5 +8,7 @@ Write-Output ' '
 #-------------------------------
 Write-Output 'Compiling...'
 . $env:USERPROFILE\.platformio\penv\Scripts\platformio.exe run -e STM32F103RC_creality
+ls '.pio\build\STM32F103RC_creality\*.bin' | Rename-Item -NewName {$_.name -replace 'firmware','Ender3S1-Beta' -replace '-[0-9]+[.]','.'}
+
 Move-Item -Path '.pio\build\STM32F103RC_creality\*.bin' -Destination '..\Releases'
 Write-Output ' '
